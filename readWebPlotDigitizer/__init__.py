bc2506b4c610c3d6bd26c3803f468508a7e61361
zhaozc <yanyuechuixue@gmail.com>
2023-11-25 15:59:07 +0800

    0.1

---
+++ readWebPlotDigitizer/__init__.py
@@ -0,0 +1,1 @@
+from .readWebPlotDigitizer import readWebPlotDigitizer
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='readWebPlotDigitizer',
+    version='0.1',
+    packages=['readWebPlotDigitizer'],
+    description='A very short code to read data from WebPlotDigitizer',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Zhi-Chao Zhao',
+    author_email='yanyuechuixue@gmail.com',
+    url='https://github.com/yanyuechuixue/readWebPlotDigitizer',
+    license='MIT',
+    install_requires=[
+        csv
+    ],
+)
